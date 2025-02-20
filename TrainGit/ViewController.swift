4a95753468df987b1447612239f40550fcc7b944
Demna Koridze <demnaqoridze@gmail.com>
2025-02-20 11:42:16 +0400

    vol 1

---
+++ TrainGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrainGit
+//
+//  Created by demnakoridze on 20.02.25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
