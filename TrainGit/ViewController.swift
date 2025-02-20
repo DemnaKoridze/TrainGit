49aaa73ad75907f143eca649c040e619371c4f80
Demna Koridze <demnaqoridze@gmail.com>
2025-02-20 12:20:42 +0400

    abc

--- TrainGit/ViewController.swift
+++ TrainGit/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         
         // this change is from part1 branch and works separately
         
-        
+        // this is a part 1 branch 
         
         
     }
